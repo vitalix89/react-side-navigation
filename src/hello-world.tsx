fcfeb5b34f4998671cfc3f888b367b1130c04465
VITALY <vitaly89@walla.com>
2019-04-13 20:55:10 +0300

    feat(HelloWorld): add interface for component props & changed class with function

--- src/hello-world.tsx
+++ src/hello-world.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
-export class HelloWorld extends React.Component<any, any> {
-  public render() {
-    return <div style={{ color: this.props.color }}>Hello world!</div>;
-  }
+interface Props {
+  /** Color of the text */
+  color: string;
+}
+
+export function HelloWorld({ color }: Props) {
+  return <div style={{ color }}>Hello world!</div>;
 }
