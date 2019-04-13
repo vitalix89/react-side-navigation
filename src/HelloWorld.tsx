1d462770cdb10f9ca725df7567c72dad4ecc29c2
VITALY <vitaly89@walla.com>
2019-04-13 18:38:06 +0300

    initial commit

---
+++ src/HelloWorld.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export class HelloWorld extends React.Component<any, any> {
+  render() {
+    return <div style={{ color: this.props.color }}>Hello world!</div>;
+  }
+}
